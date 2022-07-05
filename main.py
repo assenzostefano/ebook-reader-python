5b1860ac2c840fedad8452d66fc1f741f2dd6d45
stef-devs <stef58a@gmail.com>
2022-07-05 21:19:41 +0200

    Center text

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import tkinter
 from tkinter import *
 
+#Create window
 top = tkinter.Tk()
+
+#Title window
 top.title('Ebook reader')
+
+#Text center
+text = Label(top, text="Ebook reader", fg='red', font=("Helvetica", 16))
+text.place(anchor="center")
+text.pack()
 
 top.mainloop()
