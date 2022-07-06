acb54a203c0201fcee6dbbc3667615759ffacc4a
stef-devs <stef58a@gmail.com>
2022-07-06 14:41:10 +0200

    FInish project

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
-import tkinter as tk
-from tkinter import *
-from tkinter import Button
+import pyttsx3
+import PyPDF2
 
-
-#Create window
-tk = tk.Tk()
-tk.geometry("1280x800")
-
-#Title window
-tk.title('Ebook reader')
-
-#Text center
-text = Label(tk, text= "Ebook Reader", font= ('Helvetica 20 bold')).grid(row=300, column=500, padx= 550, pady= 300)
-
-def page1():
-    label = Label(text='this is the page1')
-    label.place(relx=0.3,rely=0.4)
-
-def page2():
-    label = Label(text='this is the page2')
-    label.place(relx=0.3,rely=0.4)
-
-bt = Button(tk,text='page1',command=page1)
-bt.grid(column=100,row=100)
-
-bt1 = Button(tk,text='page2',command=page2)
-bt1.grid(row=0,column=1)
-
-tk.mainloop()
+book = open('test.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(1, pages):
+    page = pdfReader.getPage(1)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
