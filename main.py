f4a5b8b86032aa15b1cbbf4f3c4b0e927aa1e98b
stef-devs <stef58a@gmail.com>
2022-07-06 12:09:33 +0200

    Add buton and gitignore for testing

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
+from tkinter import Button
+
 
 #Create window
-top = tkinter.Tk()
-top.geometry("1280x800")
+tk = tk.Tk()
+tk.geometry("1280x800")
 
 #Title window
-top.title('Ebook reader')
+tk.title('Ebook reader')
 
 #Text center
-text = Label(top, text= "Ebook Reader", font= ('Helvetica 20 bold')).grid(row=300, column=500, padx= 550, pady= 300)
+text = Label(tk, text= "Ebook Reader", font= ('Helvetica 20 bold')).grid(row=300, column=500, padx= 550, pady= 300)
 
-top.mainloop()
+def page1():
+    label = Label(text='this is the page1')
+    label.place(relx=0.3,rely=0.4)
+
+def page2():
+    label = Label(text='this is the page2')
+    label.place(relx=0.3,rely=0.4)
+
+bt = Button(tk,text='page1',command=page1)
+bt.grid(column=100,row=100)
+
+bt1 = Button(tk,text='page2',command=page2)
+bt1.grid(row=0,column=1)
+
+tk.mainloop()
